178eddb7020a639560c99e76721b019439958eb7
Josh Pollock <josh@calderawp.com>
2018-11-12 10:25:59 -0500

    add color controls

--- blocks/call-to-action-editable/index.js
+++ blocks/call-to-action-editable/index.js
@@ -1,10 +1,14 @@
 import {createElement} from '@wordpress/element';
 import {__} from '@wordpress/i18n';
 import {registerBlockType} from '@wordpress/blocks';
-import {TextControl,} from '@wordpress/components';
+import {
+	TextControl,
+	BaseControl
+} from '@wordpress/components';
 import {
 	InspectorControls,
-	AlignmentToolbar
+	AlignmentToolbar,
+	ColorPalette
 } from '@wordpress/editor';
 
 /**
@@ -14,24 +18,68 @@ import {
  */
 const createStyle = (styleProps) => {
 	return {
-		textAlign: styleProps.hasOwnProperty('align') ? styleProps.align : 'left'
+		textAlign: styleProps.hasOwnProperty('align') ? styleProps.align : 'left',
+		backgroundColor: styleProps.hasOwnProperty('backgroundColor') ? styleProps.backgroundColor : ''
 	}
 }
+
 /**
  * One function to generate the same content in edit/save
  *
  * @return {*}
  */
 function Content({attributes}) {
-	const {text, linkText, link,align} = attributes;
+	const {
+		text,
+		linkText,
+		link,
+		align,
+		linkColor,
+		textColor,
+		backgroundColor
+	} = attributes;
 	return (
-		<div style={createStyle({align})}>
-			<p>{text}</p>
-			<a href={link}>{linkText}</a>
+		<div
+			style={createStyle({
+				align,
+				backgroundColor
+			})}>
+			<p style={{
+				color: textColor
+			}}>
+				{text}
+			</p>
+			<a
+				href={link}
+				style={{
+					color: linkColor
+				}}>
+				{linkText}
+			</a>
 		</div>
 	);
 }
 
+
+const colors = [
+	{name: 'Green', color: '#a3bf61'},
+	{name: 'Orange', color: '#ff7e30'},
+	{name: 'Dark Grey', color: '#3a3a3c'},
+	{name: 'White', color: '#ffffff;'},
+	{name: 'Black', color: '#000000;'},
+];
+
+const ColorControl = (props) => (
+	<BaseControl
+		label={props.label}
+	>
+		<ColorPalette
+			colors={colors}
+			value={props.value}
+			onChange={props.onChange}
+		/>
+	</BaseControl>
+);
 
 
 registerBlockType('caldera-learn-basic-blocks/call-to-action-editable', {
@@ -65,17 +113,29 @@ registerBlockType('caldera-learn-basic-blocks/call-to-action-editable', {
 			default: 'https://joshpress.net'
 		},
 		align: {
-			type:'string',
+			type: 'string',
 			default: 'left'
+		},
+		textColor: {
+			type: 'string'
+		},
+		backgroundColor: {
+			type: 'string',
+		},
+		linkColor: {
+			type: 'string',
 		}
 	},
 
-	edit({attributes, setAttributes,isSelected}) {
+	edit({attributes, setAttributes, isSelected}) {
 		const {
 			text,
 			linkText,
 			link,
-			align
+			align,
+			textColor,
+			backgroundColor,
+			linkColor
 		} = attributes;
 
 
@@ -83,6 +143,11 @@ registerBlockType('caldera-learn-basic-blocks/call-to-action-editable', {
 		const onChangeLinkText = (linkText) => setAttributes({linkText});
 		const onChangeLink = (link) => setAttributes({link});
 		const onChangeAlign = (align) => setAttributes({align});
+		const onChangeTextColor = (textColor) => setAttributes({textColor});
+		const onChangeBackgroundColor = (backgroundColor) => setAttributes({backgroundColor});
+		const onChangeLinkColor = (linkColor) => setAttributes({linkColor});
+
+
 		if (isSelected) {
 			return (
 				<div>
@@ -97,6 +162,22 @@ registerBlockType('caldera-learn-basic-blocks/call-to-action-editable', {
 							value={link}
 							onChange={onChangeLink}
 						/>
+						<ColorControl
+							label={'Text Color'}
+							onChange={onChangeTextColor}
+							value={textColor}
+						/>
+						<ColorControl
+							label={'Link Color'}
+							onChange={onChangeLinkColor}
+							value={linkColor}
+						/>
+						<ColorControl
+							label={'Background Color'}
+							onChange={onChangeBackgroundColor}
+							value={backgroundColor}
+						/>
+
 					</InspectorControls>
 					<AlignmentToolbar
 						value={align}
